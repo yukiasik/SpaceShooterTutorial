30d7a2886ff912902a61a2fb254e01dc253768a8
Bacman <bacman0304@gmail.com>
2018-05-01 22:37:40 +0200

    doadanie odczytu z czujnika eda

---
+++ Assets/Scripts/BTalinoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTalinoController : MonoBehaviour
+{
+    public static BITalinoReader Reader;
+
+    private void Start()
+    {
+        Reader = GetComponent<BITalinoReader>();
+    }
+}
